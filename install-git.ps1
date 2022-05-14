b243d8be99edf9cabc9535fb107c4cb9df256c68
tsgcpp <tsgcpp@users.noreply.github.com>
2022-05-14 19:33:46 +0900

    Add install-git.ps1

---
+++ install-git.ps1
@@ -0,0 +1,38 @@
+. ./install-functions.ps1
+
+$PackageFormat = 'Git-2.*64-bit.exe'
+$LogFile = ".install-git.log.tmp"
+
+$PackageFile = Find-Install-Package-WIE -PackageFormat $PackageFormat
+if (!($?))
+{
+    exit 1
+}
+
+Write-Output "Installing $PackageFile"
+
+# See the install folder in PowerShellOptions.inf
+& ./$PackageFile `
+    /LANG=en `
+    /ALLUSERS `
+    /VERYSILENT `
+    /NORESTART `
+    /NOCANCEL `
+    /SP- `
+    /SUPPRESSMSGBOXES `
+    /CLOSEAPPLICATIONS `
+    /RESTARTAPPLICATIONS `
+    /LOG=$LogFile `
+    /LOADINF=PowerShellOptions.inf `
+    | Out-Null
+
+$Result = $?
+
+Get-Content $LogFile
+Remove-Item $LogFile
+
+if (!($Result))
+{
+    Write-Error "Installation failed."
+    exit 1
+}
