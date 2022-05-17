63b5a86548dacfd950ae19866e639323528666df
tsgcpp <tsgcpp@users.noreply.github.com>
2022-05-17 22:47:52 +0900

    Add PATH of "C:\Program Files\Git\cmd" into set-git-path.ps1

--- set-git-path.ps1
+++ set-git-path.ps1
@@ -2,4 +2,5 @@ $ErrorActionPreference = 'Stop'
 
 . ./install-functions.ps1
 
+Add-Machine-Path-If-Needed-WIE -NewPath 'C:\Program Files\Git\cmd'
 Add-Machine-Path-If-Needed-WIE -NewPath 'C:\Program Files\Git\bin'
